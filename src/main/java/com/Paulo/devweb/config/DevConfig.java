8b44e9b8aa47c757979c48c9ddb74e97cd1eaba6
Paulojosep <paulo.jmodesto@htmail.com>
2021-04-21 15:19:09 -0300

    Deploy para Heroku

---
+++ src/main/java/com/Paulo/devweb/config/DevConfig.java
@@ -0,0 +1,27 @@
+package com.Paulo.devweb.config;
+
+import com.Paulo.devweb.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instanciaBaseDeDados(){
+        if(strategy.equals("create")) {
+            dbService.iniciaBaseDeDados();
+        }
+        return false;
+    }
+}
